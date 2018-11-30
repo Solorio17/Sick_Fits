1e3f28c7f998e1d35e82742fe63dba6da6c89eb9
Jason Solorio <jasonsoloriom@gmail.com>
2018-11-29 17:11:44 -0700

    First Mutation, that allows us to add an item to the db

--- backend/src/resolvers/Mutation.js
+++ backend/src/resolvers/Mutation.js
@@ -1,3 +1,14 @@
-const mutations = {};
-
-module.exports = mutations;
+const Mutations = {
+    async createItem(parent, args, ctx, info){
+        //TODO: check if theyre logged in
+  
+        const item = await ctx.db.mutation.createItem({
+            data: {
+                ...args //same as habing to do title: args.title ...
+            }
+        }, info);
+        return item
+    }
+  };
+  
+  module.exports = Mutations;
