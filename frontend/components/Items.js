11bafad9d1aac69939d0474be35b8731ca3070d6
Jason Solorio <jasonsoloriom@gmail.com>
2018-12-03 19:51:05 -0700

    Items component will take in individual Item and format them to display in two equal columns

--- frontend/components/Items.js
+++ frontend/components/Items.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { Query } from 'react-apollo';
 import gql from 'graphql-tag';
 import styled from 'styled-components';
+import Item from './Item';
 
 const ALL_ITEMS_QUERY = gql`
     query ALL_ITEMS_QUERY{
@@ -22,7 +23,7 @@ const Center = styled.div`
 
 const ItemsList = styled.div`
     display: grid;
-    grid-template-columns: 1fr 1fr 1fr;
+    grid-template-columns: 1fr 1fr;
     grid-gap: 60px;
     max-width: ${props => props.theme.maxWidth};
 `;
@@ -36,7 +37,7 @@ class Items extends Component{
                         if(loading) return <p>Loading...</p>
                         if(error) return <p>Error: {error.message}</p>
                         return <ItemsList>
-                            {data.items.map(item => <p>{item.title}</p>)}
+                            {data.items.map(item => <Item item={item} key={item.id}/>)}
                         </ItemsList>
                     }}
                 </Query>
