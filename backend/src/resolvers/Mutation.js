e74a26144f6b48776f0f0080b61ecec01ab72f8d
Jason Solorio <jasonsoloriom@gmail.com>
2018-12-05 21:18:01 -0700

    New mutation deleteItem, works by querying an item based on its id, and then calling a mutation onto deleteItem.

--- backend/src/resolvers/Mutation.js
+++ backend/src/resolvers/Mutation.js
@@ -19,6 +19,15 @@ const Mutations = {
         id: args.id
       }
     }, info)
+  },
+  async deleteItem(parent, args, ctx, info){
+    const where = {id: args.id };
+    //1. Find the item
+    const item = await ctx.db.query.item({where}, `{ id title}`);
+    //2. Check if the user owns that item or have permission
+    // TODO
+    //3. Delete item
+    return ctx.db.mutation.deleteItem({ where }, info);
   }
 };
   
