abc8555ccd63b4eb80da7f48655570fe1e916d3f
Jason Solorio <jasonsoloriom@gmail.com>
2018-12-08 20:01:57 -0700

    New Mutations; signup, signin, and signout. Logic of signup is first to convert any email to lowercase in the database, second is to hash the password with bcrypt-- passing in a 10 character salt, third is to create the user and save it to the db with a mutation, inside the mutation we pass in all the arguments and give a user permission of USER. Lastly create a JWT to the user, taking advantage the users id-- set the token as a cookie and give it a year to expire.

--- backend/src/resolvers/Mutation.js
+++ backend/src/resolvers/Mutation.js
@@ -1,3 +1,6 @@
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+
 const Mutations = {
   async createItem(parent, args, ctx, info){
     //TODO: check if theyre logged in
@@ -28,6 +31,56 @@ const Mutations = {
     // TODO
     //3. Delete item
     return ctx.db.mutation.deleteItem({ where }, info);
+  },
+  async signup(parent, args, ctx, info){
+    //lowercase email
+    args.email = args.email.toLowerCase();
+    //hash user password
+    const password = await bcrypt.hash(args.password, 10) //10 is SALT
+    //create user in db
+    const user = await ctx.db.mutation.createUser(
+      {
+        data: {
+          ...args,
+          password: password,
+          permissions: { set: ['USER']}
+        },
+      },
+      info);
+      //create JWT for user
+      const token = jwt.sign({ userId: user.id}, process.env.APP_SECRET);
+      //set JWT as cookie on the response
+      ctx.response.cookie('token', token, {
+        httpOnly: true,
+        maxAge: 1000 * 60 * 60 * 24 * 365, //1 year cookie
+      });
+      //return user to browser
+      return user;
+  },
+  async signin(parent, {email, password}, ctx, info){
+    //1. check if there's a user with that email
+    const user = await ctx.db.query.user({ where: { email: email }});
+    if(!user){
+      throw new Error(`No user found with email ${email}`);
+    }
+    //2. check if their password is correct
+    const valid = await bcrypt.compare(password, user.password);
+    if(!valid){
+      throw new Error('Invalid password!');
+    }
+    //3. generate JWT
+    const token = jwt.sign({ userId: user.id}, process.env.APP_SECRET)
+    //4. set cookie with token
+    ctx.response.cookie('token', token, {
+      httpOnly: true,
+      maxAge: 1000 * 60 * 60 * 24 * 365,
+    });
+    //5. return user
+    return user;
+  },
+  signout(parent, args, ctx, info){
+    ctx.response.clearCookie('token');
+    return { message: "Goodbye old friend.."}
   }
 };
   
