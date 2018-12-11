e7a758b573ea6ca96567d756827d21a23a3007f1
Jason Solorio <jasonsoloriom@gmail.com>
2018-12-11 16:16:22 -0700

    Checking if a user is logged in, before they can create an item. Using the ctx.request to access the userId. And when there is a user, create a relationship between the item and user

--- backend/src/resolvers/Mutation.js
+++ backend/src/resolvers/Mutation.js
@@ -6,10 +6,18 @@ const { transport, makeAnEmail } = require('../mail');
 
 const Mutations = {
   async createItem(parent, args, ctx, info){
-    //TODO: check if theyre logged in
+    //check if they're logged in
+    if(!ctx.request.userId){
+      throw new Error('You must be logged in to do that!')
+    }
 
     const item = await ctx.db.mutation.createItem({
       data: {
+        user:{ //creates relationship between item and user through userId
+          connect: {
+            id: ctx.request.userId 
+          },
+        },
         ...args
       }
     }, info)
