83476e389d7e5cfd2ba7e33fbc10f3e60b801aaa
Jason Solorio <jasonsoloriom@gmail.com>
2018-12-12 15:21:54 -0700

    Wrapping the CartStyles component with a query to bring in local state, and another mutation to toggle the cart

--- frontend/components/Cart.js
+++ frontend/components/Cart.js
@@ -3,23 +3,44 @@ import CartStyles from './styles/CartStyles';
 import SickButton from './styles/SickButton';
 import Supreme from './styles/Supreme';
 import CloseButton from './styles/CloseButton';
+import gql from 'graphql-tag';
+import { Query, Mutation } from 'react-apollo';
+
+const LOCAL_STATE_QUERY = gql`
+    query{
+        cartOpen @client
+    }
+`;
+
+const TOGGLE_CART_MUTATION = gql`
+    mutation{
+        toggleCart @client
+    }
+`;
 
 const Cart = () => {
     return(
-        <CartStyles open>
-            <header>
-                <CloseButton title="close">X</CloseButton>
-                <Supreme>Your Cart</Supreme>
-                <p>You have 1 item in your cart</p>
-            </header>
+        <Mutation mutation={TOGGLE_CART_MUTATION}>{(toggleCart) => (
+            <Query query={LOCAL_STATE_QUERY}>{({data}) => (
+                <CartStyles open={data.cartOpen}>
+                    <header>
+                        <CloseButton onClick={toggleCart} title="close">X</CloseButton>
+                        <Supreme>Your Cart</Supreme>
+                        <p>You have 1 item in your cart</p>
+                    </header>
 
-            <footer>
-                <p>$1,200</p>
-                <SickButton>Checkout</SickButton>
-            </footer>
+                    <footer>
+                        <p>$1,200</p>
+                        <SickButton>Checkout</SickButton>
+                    </footer>
 
-        </CartStyles>
+                </CartStyles>
+            )}
+        </Query>
+        )}
+      </Mutation>
     )
 }
 
 export default Cart;
+export { LOCAL_STATE_QUERY, TOGGLE_CART_MUTATION };
