234b410ecd0b1e73cb664ab719e49b18e3ed3fa7
Jason Solorio <jasonsoloriom@gmail.com>
2018-12-11 18:11:16 -0700

    New query users, that first checks for login status, then checks if a user has a certain permission that allows the user to view all user permissions, if so then return all users.

--- backend/src/resolvers/Query.js
+++ backend/src/resolvers/Query.js
@@ -1,4 +1,5 @@
 const { forwardTo } = require('prisma-binding');
+const { hasPermission } = require('../utils');
 
 const Query = {
 
@@ -16,6 +17,18 @@ const Query = {
             },
             info
         )
+    },
+    async users(parent, args, ctx, info){
+        //1. check if they're logged in
+        if(!ctx.request.userId){
+            throw new Error('You must be logged in.')
+        }
+        //2. check if user has permission to query all users
+        hasPermission(ctx.request.user, ['ADMIN', 'PERMISSIONUPDATE'])
+
+        //3. if they do, query all users
+        return ctx.db.query.users({}, info)
+
     }
     // async items(parent, args, ctx, info){
     //     const items = await ctx.db.query.items();
