1e9076b7084864ba7c1cce7c1d8216134a63fff0
Jason Solorio <jasonsoloriom@gmail.com>
2018-12-11 22:34:03 -0700

    Supreme styling

--- frontend/components/styles/Supreme.js
+++ frontend/components/styles/Supreme.js
@@ -1,13 +1,11 @@
 import styled from 'styled-components';
 
 const Supreme = styled.h3`
-  background: ${props => props.theme.red};
-  color: white;
+  color: black;
   display: inline-block;
   padding: 4px 5px;
-  transform: skew(-3deg);
   margin: 0;
-  font-size: 4rem;
+  font-size: 32px;
 `;
 
 export default Supreme;
