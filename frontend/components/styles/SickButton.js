45147e4b56e17bbfe63d09b9f4568c4ef67809b1
Jason Solorio <jasonsoloriom@gmail.com>
2018-12-11 22:34:29 -0700

    SickButton styling

--- frontend/components/styles/SickButton.js
+++ frontend/components/styles/SickButton.js
@@ -6,10 +6,8 @@ const SickButton = styled.button`
   font-weight: 500;
   border: 0;
   border-radius: 0;
-  text-transform: uppercase;
-  font-size: 2rem;
+  font-size: 22px;
   padding: 0.8rem 1.5rem;
-  transform: skew(-2deg);
   display: inline-block;
   transition: all 0.5s;
   &[disabled] {
