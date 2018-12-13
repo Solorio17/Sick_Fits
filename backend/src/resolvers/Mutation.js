c8b2ff4c13282eeac2958e414180837cbe75fd6a
Jason Solorio <jasonsoloriom@gmail.com>
2018-12-12 22:27:07 -0700

    New mutation removeFromCart-- First we find the item waiting to be deleted, then we make sure we found that item, next make sure the users owns the item they're trying to delete, and if so then delete the item

--- backend/src/resolvers/Mutation.js
+++ backend/src/resolvers/Mutation.js
@@ -221,6 +221,26 @@ const Mutations = {
         }
       }
     }, info)
+  },
+  async removeFromCart(parent, args, ctx, info){
+    //find cart item
+    const cartItem = await ctx.db.query.cartItem({
+      where: {
+        id: args.id
+      }
+    }, `{id, user {id}}`)
+    //make sure we found item
+    if(!cartItem) throw new Error('No CartItem found!')
+    //make sure user owns that cart item
+    if(cartItem.user.id !== ctx.request.userId){
+      throw new Error('You don\'t own that cart item')
+    }
+    //delete cart item
+    return ctx.db.mutation.deleteCartItem({
+      where: {
+        id: args.id
+      }
+    }, info)
   }
 };
   
