c9f0e9bc3e14309f00471ecee0b1a4cb2cf36d90
Jason Solorio <jasonsoloriom@gmail.com>
2018-12-12 20:26:52 -0700

    New component AddToCart.js-- Uses a new mutation addToCart, which takes in an id and returns another id and a quantity. The purpose of this component is to allow a user to add an item to their cart.

---
+++ frontend/components/AddToCart.js
@@ -0,0 +1,32 @@
+import React, { Component } from 'react';
+import gql from 'graphql-tag';
+import { Query, Mutation } from 'react-apollo';
+import { CURRENT_USER_QUERY } from './User';
+
+const ADD_TO_CART_MUTATION = gql`
+    mutation addToCart($id: ID!){
+        addToCart(id: $id){
+            id
+            quantity
+        }
+    }
+`;
+
+class AddToCart extends Component{
+    render(){
+        const { id } = this.props
+        return(
+            <Mutation 
+                mutation={ADD_TO_CART_MUTATION} 
+                variables={{
+                    id: id
+                }}
+                refetchQueries={[{ query: CURRENT_USER_QUERY }]}
+            >
+                {(addToCart, { error, loading}) => <button onClick={addToCart} disabled={loading}>Add{loading && 'ing'} to cart</button>}
+            </Mutation>
+        )
+    }
+}
+
+export default AddToCart;
