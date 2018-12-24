f8b7c0a0f2894225f5698d8c2744afc174744ef7
Jason Solorio <jasonsoloriom@gmail.com>
2018-12-23 17:34:27 -0700

    Mocking the request and result for the CURRENT_USER_QUERY and mocking the request and result for the REMOVE_FROM_CART_MUTATION. Writing a snapshot for the whole component and testing to see if an item can be removed from a users cart.

---
+++ frontend/__tests__/RemoveFromCart.test.js
@@ -0,0 +1,72 @@
+import { mount } from 'enzyme';
+import wait from 'waait';
+import toJSON from 'enzyme-to-json';
+import { MockedProvider } from 'react-apollo/test-utils';
+import RemoveFromCart, {REMOVE_FROM_CART_MUTATION} from '../components/RemoveFromCart';
+import { fakeCartItem, fakeUser } from '../lib/testUtils';
+import { ApolloConsumer} from 'react-apollo';
+import {CURRENT_USER_QUERY} from '../components/User';
+
+global.alert = jest.fn();
+
+const mocks = [
+    {
+        request: { query: CURRENT_USER_QUERY },
+        result: {
+            data: {
+                me: {
+                    ...fakeUser(),
+                    cart: [fakeCartItem({ id: 'abc123'})]
+                }
+            }
+        }
+    },
+    {
+        request: { query: REMOVE_FROM_CART_MUTATION, variables: {id: 'abc123'} },
+        result: {
+            data: {
+                removeFromCart: {
+                    __typename: 'CartItem',
+                    id: 'abc123'
+                }
+            }
+        }
+    },
+];
+
+
+describe('RemoveFromCart component', () => {
+    it('renders and matches snapshot', async()=>{
+        const wrapper = mount(
+            <MockedProvider mocks={mocks}>
+                <RemoveFromCart id={'abc123'}/>
+            </MockedProvider>
+        );
+        expect(toJSON(wrapper.find('button'))).toMatchSnapshot();
+    });
+
+    it('removes the item from cart', async()=>{
+        let apolloClient;
+
+        const wrapper = mount(
+            <MockedProvider mocks={mocks}>
+                <ApolloConsumer>
+                    {(client)=>{
+                        apolloClient = client;
+                        return <RemoveFromCart id={'abc123'}/>
+                    }}
+                </ApolloConsumer>
+            </MockedProvider>
+        );
+        const res = await apolloClient.query({ query: CURRENT_USER_QUERY });
+        expect(res.data.me.cart).toHaveLength(1);
+        expect(res.data.me.cart[0].item.price).toBe(5000);
+        wrapper.find('button').simulate('click');
+        await wait();
+        wrapper.update()
+        const res2 = await apolloClient.query({ query: CURRENT_USER_QUERY });
+        expect(res2.data.me.cart).toHaveLength(0);
+        
+    })
+});
+
